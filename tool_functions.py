7f2e6f1177de7e264b9d7689439fcc8daaac6bcc
Mona Sidhoum <mona.sidhoum@etu.minesparis.psl.eu>
2026-06-29 15:07:37 +0200

    petits changements bis

--- tool_functions.py
+++ tool_functions.py
@@ -7,4 +7,5 @@ def lire_planning_excel(chemin_fichier: str) -> str :
     """ Utile pour lire le contenu du fichier Excel """
     return pd.read_excel(chemin_fichier).to_string(index=False)
 
-planning = lire_planning_excel("/data/Export_Planning_du_12_01_2026_au_16_01_2026.xlsx")
+#planning = lire_planning_excel.invoke({"chemin_fichier": "./data/Export_Planning_du_12_01_2026_au_16_01_2026.xlsx"})
+#print(planning)
