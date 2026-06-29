d4758c12cbe747a0d2e5d383aa766ab026593711
Noah Bouter <noah.boutter@etu.minesparis.psl.eu>
2026-06-29 14:57:03 +0200

    read schedule

--- tool_functions.py
+++ tool_functions.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from langchain.tools import tool
 
+
 @tool
 def lire_planning_excel(chemin_fichier: str) -> str :
     """ Utile pour lire le contenu du fichier Excel """
     return pd.read_excel(chemin_fichier).to_string(index=False)
+
+planning = lire_planning_excel("/data/Export_Planning_du_12_01_2026_au_16_01_2026.xlsx")
