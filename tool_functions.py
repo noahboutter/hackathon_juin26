c82deb691f6508bbbe8a6c0dd8a7b5ea6891b8c7
Noah Bouter <noah.boutter@etu.minesparis.psl.eu>
2026-06-29 16:01:14 +0200

    change columns + prompt

--- tool_functions.py
+++ tool_functions.py
@@ -6,7 +6,9 @@ def lire_planning_excel_df(chemin_fichier: str):
     """ Utile pour lire le contenu du fichier Excel """
     if platform.system() == "Darwin":
         chemin_fichier.replace("/", "\\")
-    return pd.read_excel(chemin_fichier)
+    df = pd.read_excel(chemin_fichier)
+    df.rename(inplace=True, columns={'Identifiant':'id', 'Heure de fin':'H_fin', 'Heure de début':'H_deb', 'Type de journée':'type_jour', 'Code journée' : 'code_jour'})
+    return df
 
 planning_df = lire_planning_excel_df("/data/Export_Planning_du_12_01_2026_au_16_01_2026.xlsx")
 
