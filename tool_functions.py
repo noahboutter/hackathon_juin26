09e07d6197b07a73b9465719f0e46da7db59ba0c
Noah Bouter <noah.boutter@etu.minesparis.psl.eu>
2026-06-29 15:08:54 +0200

    read_file change

--- tool_functions.py
+++ tool_functions.py
@@ -1,10 +1,15 @@
 import pandas as pd
 from langchain.tools import tool
+import platform
 
 
 @tool
 def lire_planning_excel(chemin_fichier: str) -> str :
     """ Utile pour lire le contenu du fichier Excel """
+    if platform.system() == "Darwin":
+        chemin_fichier.replace("/", "\\")
     return pd.read_excel(chemin_fichier).to_string(index=False)
 
 planning = lire_planning_excel("/data/Export_Planning_du_12_01_2026_au_16_01_2026.xlsx")
+
+
