86cb70abb1a9f416a95c6a6575ade6f846900f8d
Noah Bouter <noah.boutter@etu.minesparis.psl.eu>
2026-06-29 16:59:47 +0200

    last push du day

--- tool_functions.py
+++ tool_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from langchain.tools import tool
 import platform
+import datetime
 
 def lire_planning_excel_df(chemin_fichier: str):
     """ Utile pour lire le contenu du fichier Excel """
@@ -11,7 +12,7 @@ def lire_planning_excel_df(chemin_fichier: str):
     return df
 
 planning_df = lire_planning_excel_df("data/Export_Planning_du_12_01_2026_au_16_01_2026.xlsx")
-non_affecte_dfs = [lire_planning_excel_df(f"Services Agents non affectés le 1{i}_01_2026.xlsx") for i in range(2, 7)]
+non_affecte_dfs = [lire_planning_excel_df(f"data/Services_Agents_non_affectés_le_1{i}_01_2026.xlsx") for i in range(2, 7)]
 
 @tool
 def lire_planning_excel(chemin_fichier: str) -> str :
@@ -29,6 +30,15 @@ def get_machinistes(ligne, df=planning_df):
         if str(ligne) in str(df.loc[id, "Qualification : Connaissance de ligne"]):
             machinistes.append(id)
     return machinistes
+
+def get_fitting_agents(day: str, deb="00:00", fin="23:59", lines=None) -> str:
+    day_index = datetime(day).day % 10 - 2
+    deb_time = datetime(deb)
+    fin_time = datetime(fin)
+    df = non_affecte_dfs[day_index]
+    mask = (datetime(df['Début']) > deb_time) & (datetime(df["Fin'"]) < fin_time) & (int(df['Service'].str[1:3]) in lines)
+    return df[mask]['Service'].to_string()
+
 
 
 
